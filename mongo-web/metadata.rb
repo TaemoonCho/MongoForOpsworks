546e60c3f361236f1ba192eae8dcf8a85551beba
Taemoon Jo <whxoans@gmail.com>
2015-01-20 16:30:58 +0900

    Setting done

---
+++ mongo-web/metadata.rb
@@ -0,0 +1,7 @@
+name        "mongodb-for-web"
+description 'Cookbook for MongoDB Berkshelf based install'
+maintainer  "TaemoonJo"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
